c7ba89444d160cb81656a49cb93416ee5013aa8f
spisarski <s.pisarski@cablelabs.com>
2017-06-22 12:43:09 -0600

    Use neutron to create floating IPs.
    This patch moves the floating IP creation out of nova and into neutron.
    Other changes include the use of domain objects for VM and Floating IP
    instances, addition of new nova_utils tests to exercise the create
    server functionality, and more PEP8 compliance.
    
    JIRA: SNAPS-92
    
    Change-Id: I16c12b26b56008901633e90ae307586ad2045f9b
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

---
+++ snaps/domain/test/vm_inst_tests.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2017 Cable Television Laboratories, Inc. ("CableLabs")
+#                    and others.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from snaps.domain.vm_inst import VmInst, FloatingIp
+
+
+class VmInstDomainObjectTests(unittest.TestCase):
+    """
+    Tests the construction of the snaps.domain.test.Image class
+    """
+
+    def test_construction_positional(self):
+        vm_inst = VmInst('name', 'id')
+        self.assertEqual('name', vm_inst.name)
+        self.assertEqual('id', vm_inst.id)
+
+    def test_construction_named(self):
+        vm_inst = VmInst(inst_id='id', name='name')
+        self.assertEqual('name', vm_inst.name)
+        self.assertEqual('id', vm_inst.id)
+
+
+class FloatingIpDomainObjectTests(unittest.TestCase):
+    """
+    Tests the construction of the snaps.domain.test.Image class
+    """
+
+    def test_construction_positional(self):
+        vm_inst = FloatingIp('id-123', '10.0.0.1')
+        self.assertEqual('id-123', vm_inst.id)
+        self.assertEqual('10.0.0.1', vm_inst.ip)
+
+    def test_construction_named(self):
+        vm_inst = FloatingIp(ip='10.0.0.1', inst_id='id-123')
+        self.assertEqual('id-123', vm_inst.id)
+        self.assertEqual('10.0.0.1', vm_inst.ip)
