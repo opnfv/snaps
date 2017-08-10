dc6d1cf2c179220226bda7fd2475c74693063a75
spisarski <s.pisarski@cablelabs.com>
2017-08-10 09:17:44 -0600

    Scripts to allow Jenkins to run the SNAPS-OO tests.
    JIRA: SNAPS-80
    
    Change-Id: I7d634a5484752194d7796e841233bf387c1aeafe
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

---
+++ ci/run_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Setup Python runtime
+sudo pip install virtualenv
+virtualenv ./vpy
+source ./vpy/bin/activate
+pip install -e ../
+
+# $1 is the IP to the pod's build server
+# $2 is the IP to the pod's control server
+
+# This operation installs squid on the pod's build server
+ansible-playbook -i ${1}, setup_proxy.yaml
+
+# Get RC file from control server
+filename=$(ssh -o StrictHostKeyChecking=no root@${2} find /var/lib/lxc/controller00_nova_api_placement_container-* -name openrc)
+scp root@${2}:${filename} .
+
+# Execute tests
+python ../snaps/test_runner.py -e openrc -n public -ci -p ${1}:3128
+
+# Cleanup virtual python runtime
+rm -rf ./vpy
