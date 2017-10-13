24f6b7a866e5b7461db21b98691d42bbb5501089
Taseer Ahmed <taseer94@gmail.com>
2017-10-13 12:34:00 +0500

    Add reason for stack creation failure
    JIRA: SNAPS-190
    
    Change-Id: I59b7d416ef16cd1f301ccbdcdd8a7529527b3dd6
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- snaps/openstack/tests/create_stack_tests.py
+++ snaps/openstack/tests/create_stack_tests.py
@@ -30,7 +30,7 @@ import unittest
 import uuid
 
 from snaps.openstack import create_stack
-from snaps.openstack.create_stack import StackSettings, StackSettingsError
+from snaps.openstack.create_stack import StackSettings, StackSettingsError, StackCreationError
 from snaps.openstack.tests import openstack_tests, create_instance_tests
 from snaps.openstack.tests.os_source_file_test import OSIntegrationTestCase
 from snaps.openstack.utils import heat_utils, neutron_utils, nova_utils
@@ -487,6 +487,9 @@ class CreateStackNegativeTests(OSIntegrationTestCase):
         self.stack_creator = None
         self.heat_tmplt_path = pkg_resources.resource_filename(
             'snaps.openstack.tests.heat', 'test_heat_template.yaml')
+        self.heat_tmplt_neg_path = pkg_resources.resource_filename(
+            'snaps.openstack.tests.heat', 'test_heat_negative_template.yaml'
+        )
 
     def tearDown(self):
         if self.stack_creator:
@@ -513,4 +516,15 @@ class CreateStackNegativeTests(OSIntegrationTestCase):
         self.stack_creator = create_stack.OpenStackHeatStack(self.heat_creds,
                                                              stack_settings)
         with self.assertRaises(IOError):
+            self.stack_creator.create()
+
+    def test_stack_status_reason(self):
+        """
+        Expect an StackCreationError when the stack creation fails
+        """
+        stack_settings = StackSettings(name=self.stack_name,
+                                       template_path=self.heat_tmplt_neg_path)
+        self.stack_creator = create_stack.OpenStackHeatStack(self.heat_creds,
+                                                             stack_settings)
+        with self.assertRaises(StackCreationError):
             self.stack_creator.create()
