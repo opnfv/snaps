9b6afb3e525c60109efd098c7cb864b25253c7e0
Taseer Ahmed <taseer94@gmail.com>
2017-10-13 12:34:00 +0500

    Add reason for stack creation failure
    JIRA: SNAPS-190
    
    Change-Id: I59b7d416ef16cd1f301ccbdcdd8a7529527b3dd6
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- snaps/openstack/create_stack.py
+++ snaps/openstack/create_stack.py
@@ -107,9 +107,14 @@ class OpenStackHeatStack(OpenStackCloudObject, object):
                     self.stack_settings.name)
                 return self.__stack
             else:
+                status = heat_utils.get_stack_status_reason(self.__heat_cli,
+                                                            self.__stack['stack']['id'])
                 raise StackCreationError(
-                    'Stack was not created or activated in the alloted amount '
-                    'of time')
+                    'ERROR: STACK CREATION FAILED: ' + status)
+        else:
+            logger.info('Did not create stack due to cleanup mode')
+
+        return self.__stack
 
     def clean(self):
         """
