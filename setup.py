ff6141ac35b4e0abd551c4d1f08a1e62ab538ec3
Cédric Ollivier <cedric.ollivier@orange.com>
2017-10-30 14:32:14 +0100

    Sync requirements with OpenStack Pike
    OPNFV F-release is built on top of OpenStack Pike.
    Functest depends on SNAPS and requires this update.
    
    It conforms with the requirement management cleant for E-release [1].
    
    [1] https://wiki.opnfv.org/display/functest/Requirements+management
    
    Change-Id: Ifff3db55f8b2f03a91b43fb542328300c7b7b4a6
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- setup.py
+++ setup.py
@@ -7,11 +7,17 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 
-from setuptools import setup
+import setuptools
 
 __author__ = 'spisarski'
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
-setup(
-    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    pbr=True,
-)
+setuptools.setup(
+    setup_requires=['pbr>=2.0.0'],
+    pbr=True)
