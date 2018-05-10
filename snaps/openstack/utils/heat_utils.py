64a5dab9df791b62a4e2cac3ad3ca90154809da3
mbeierl <mark.beierl@dell.com>
2018-05-10 16:16:25 -0400

    Adds Stack Update
    Adds function to allow stack update to
    occur.
    
    Includes higher level object calls for stack
    update.
    
    Added blocking parameter to create() and update()
    
    Change-Id: I4558befb3ea8ea7982faff79d1ebb54fbb3d44a7
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- snaps/openstack/utils/heat_utils.py
+++ snaps/openstack/utils/heat_utils.py
@@ -144,6 +144,28 @@ def create_stack(heat_cli, stack_settings):
     return get_stack_by_id(heat_cli, stack_id=stack['stack']['id'])
 
 
+def update_stack(heat_cli, stack, env_vals):
+    """
+    Updates the specified parameters in the stack
+    :param heat_cli: the OpenStack heat client object
+    :param stack_settings: the stack configuration
+    :return: the Stack domain object
+    """
+    args = dict()
+
+    args['stack_name'] = stack.name
+    args['existing'] = True
+
+    if env_vals:
+        args['parameters'] = env_vals
+        os_stack = heat_cli.stacks.update(**args)
+        return get_stack_by_id(heat_cli, stack_id=os_stack['stack']['id'])
+    else:
+        logger.warn('Stack not updated, env_vals are None')
+        return get_stack_by_id(heat_cli, stack_id=stack.id)
+
+
+
 def delete_stack(heat_cli, stack):
     """
     Deletes the Heat stack
