bc000dc95ec1012f9c9b1c1e89eed433f6697f8f
mbeierl <mark.beierl@dell.com>
2018-06-15 16:01:39 -0400

    Adds Threads to Heat Calls
    Adds thread pool and coverts looping calls to OpenStack to be
    submitted to it, allowing them to be run in parallel.
    
    Places a maximum size of 15 concurrent calls on the thread
    pool.
    
    Change-Id: Id209c5fe9b7ee7d755d3210a124b38fc3234f0d7
    Signed-off-by: mbeierl <mark.beierl@dell.com>

--- snaps/openstack/utils/__init__.py
+++ snaps/openstack/utils/__init__.py
@@ -12,4 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from multiprocessing.pool import ThreadPool
+
 __author__ = 'spisarski'
+# Define a thread pool with a limit for how many simultaneous API requests
+# can be in progress at once.
+worker_pool = ThreadPool(processes=15)
